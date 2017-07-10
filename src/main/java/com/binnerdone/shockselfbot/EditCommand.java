4317cce2baeb59aa8a6fc0da65ffe562598577e4
William Oldham <wegg7250@gmail.com>
2017-07-10 20:49:35 +0100

    Make changes

---
+++ src/main/java/com/binnerdone/shockselfbot/EditCommand.java
@@ -0,0 +1,37 @@
+package com.binnerdone.shockselfbot;
+
+import com.walshydev.jba.commands.Command;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+
+/**
+ * Created by William on 10/07/2017.
+ * Project ShockSelfBot
+ */
+public class EditCommand implements Command {
+
+    private String command;
+    private String message;
+
+    EditCommand(String command, String message) {
+        this.command = command;
+        this.message = message;
+    }
+
+    @Override
+    public void onCommand(User user, MessageChannel messageChannel, Message message, String[] strings, Member member) {
+        message.editMessage(this.message).queue();
+    }
+
+    @Override
+    public String getCommand() {
+        return command;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
